3432fcc4d2fa1dd5a5881700a4de6c7c88794731
Lourival Linard <l.linard@gmail.com>
2021-01-27 09:24:42 -0300

    3. Criando projeto producer

---
+++ src/test/java/com/example/demospring/producer/SpringProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospring.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
